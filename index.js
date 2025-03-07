da594bbc286c78e4ad098a155fdc92f1cbaae162
SunitaSBhat <ssbh888@gmail.com>
2025-03-07 17:38:15 +0530

    This is commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const app=express();
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-app.use(express.static('public'))
+app.use(express.static('./public'))
 const PORT =4000;
 app.use("/", router);
 app.listen(PORT, ()=>{
